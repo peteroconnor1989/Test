4fa8b2fe07f918f2985dd902880c8a7f64e5f252
macasey <macasey@FSGW72328A.fexco.ie>
2015-01-21 15:30:10 +0000

    Add another hello world

--- HelloWorld/src/Test.java
+++ HelloWorld/src/Test.java
@@ -5,6 +5,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Hello World");
+		System.out.println("Hello World");
 		//a change
 	}
 
